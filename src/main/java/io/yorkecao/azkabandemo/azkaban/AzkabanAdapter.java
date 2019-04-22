8078d41866fd44c4ae62359c846ce3c8919b1497
Yorke <yorkecao@gmail.com>
2019-04-22 15:49:14 +0800

    封装新接口，优化旧接口

--- src/main/java/io/yorkecao/azkabandemo/azkaban/AzkabanAdapter.java
+++ src/main/java/io/yorkecao/azkabandemo/azkaban/AzkabanAdapter.java
@@ -102,24 +102,25 @@ public class AzkabanAdapter {
      * @return 删除结果
      */
     public void deleteProject(String projectName) {
-        HttpHeaders httpHeaders = getAzkabanHeaders();
-        httpHeaders.add("Accept", "text/plain;charset=utf-8");
         Map<String, Object> params = new HashMap<>();
         params.put("id", SESSION_ID);
         params.put("project", projectName);
 
-        restTemplate.getForObject(config.getUrl() + "/manager?session.id={id}&delete=true&project={project}", String.class, params);
-        log.info("Azkaban delete project: {}", projectName);
+        try {
+            restTemplate.getForObject(config.getUrl() + "/manager?session.id={id}&delete=true&project={project}", String.class, params);
+            log.info("Azkaban delete project: {}", projectName);
+        } catch (Exception e) {
+            log.error(String.format("Azkaban delete Project %s failure!", projectName), e);
+        }
     }
 
     /**
-     * 上传 Zip
+     * 为项目上传 Zip 文件
      *
-     * @param zipFilePath zip路径
      * @param projectName 项目名称
-     * @return 结果
+     * @param zipFilePath zip路径
      */
-    public void uploadZip(String zipFilePath, String projectName) {
+    public void uploadZip(String projectName, String zipFilePath) {
         FileSystemResource file = new FileSystemResource(new File(zipFilePath));
         LinkedMultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
         params.add("session.id", SESSION_ID);
@@ -129,9 +130,8 @@ public class AzkabanAdapter {
 
         String respResult = restTemplate.postForObject(config.getUrl() + "/manager", params, String.class);
 
-        JsonNode respRoot = null;
         try {
-            respRoot = objectMapper.readTree(respResult);
+            JsonNode respRoot = objectMapper.readTree(respResult);
             if (!respRoot.hasNonNull("error")) {
                 log.info("Azcaban Upload a Project Zip to {}: {}", projectName, zipFilePath);
             } else {
@@ -140,6 +140,7 @@ public class AzkabanAdapter {
             }
         } catch (IOException e) {
             log.error(String.format("Azcaban upload Project Zip to %s failure: %s", projectName, e.getMessage()));
+            throw new AzkabanException(e.getMessage());
         }
     }
 
@@ -150,9 +151,6 @@ public class AzkabanAdapter {
      * @return 结果
      */
     public JsonNode fetchProjectFlows(String projectName) {
-        HttpHeaders httpHeaders = getAzkabanHeaders();
-        httpHeaders.add("Accept", "text/plain;charset=utf-8");
-
         Map<String, String> params = new HashMap<>();
         params.put("id", SESSION_ID);
         params.put("project", projectName);
@@ -166,6 +164,9 @@ public class AzkabanAdapter {
                 return respRoot;
             } else {
                 String errorMessage = respRoot.get("error").asText();
+                if (errorMessage.endsWith("doesn't exist.")) {
+                    return null;
+                }
                 log.error(String.format("Azkaban fetch flows of Project {} failure: {}", projectName, errorMessage));
                 throw new AzkabanException(errorMessage);
             }
@@ -190,9 +191,8 @@ public class AzkabanAdapter {
         return exchange.toString();
     }
 
-    public String fetchFlowExecutions(String projectName, String flowId, int start, int length) {
+    public JsonNode fetchFlowExecutions(String projectName, String flowId, int start, int length) {
         HttpHeaders httpHeaders = getAzkabanHeaders();
-        httpHeaders.add("Accept", "text/plain;charset=utf-8");
         Map<String, Object> map = new HashMap<>();
         map.put("id", SESSION_ID);
         map.put("project", projectName);
@@ -200,11 +200,24 @@ public class AzkabanAdapter {
         map.put("start", start);
         map.put("length", length);
 
-        ResponseEntity<String> exchange = restTemplate.exchange(config.getUrl() + "/manager?session.id={id}&ajax=fetchFlowExecutions&project={project}&flow={flow}&start={start}&length={length}", HttpMethod.GET,
-                new HttpEntity<String>(httpHeaders), String.class, map);
+        String respResult = restTemplate.getForObject(config.getUrl() + "/manager?session.id={id}&ajax=fetchFlowExecutions&project={project}&flow={flow}&start={start}&length={length}", String.class, map);
 
-        log.info("Azkban fetch Executions of a Flow:{}", exchange);
-        return exchange.toString();
+        try {
+            JsonNode respRoot = objectMapper.readTree(respResult);
+            if (!respRoot.hasNonNull("error")) {
+                return respRoot;
+            } else {
+                String errorMessage = respRoot.get("error").asText();
+                if (errorMessage.endsWith("doesn't exist.")) {
+                    return null;
+                }
+                log.error("Azkaban fetch Executions of Flow {} failure: {}", flowId, errorMessage);
+                throw new AzkabanException(errorMessage);
+            }
+        } catch (IOException e) {
+            log.error(String.format("Azkaban fetch Executions of Flow %s failure: %s", flowId, e.getMessage()), e);
+            throw new AzkabanException(e.getMessage());
+        }
     }
 
     public String fetchFlowRunningExecutions(String projectName, String flowId) {
@@ -220,6 +233,26 @@ public class AzkabanAdapter {
 
         log.info("Azkban fetch Running Executions of a Flow:{}", exchange);
         return exchange.toString();
+    }
+
+    public void simpleExecuteFlow(String project, String flow) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", SESSION_ID);
+        map.put("project", project);
+        map.put("flow", flow);
+
+        String respResult = restTemplate.getForObject(config.getUrl() + "/executor?session.id={id}&ajax=executeFlow&project={project}&flow={flow}", String.class, map);
+        try {
+            JsonNode respRoot = objectMapper.readTree(respResult);
+            if (respRoot.hasNonNull("error")) {
+                String errorMessage = respRoot.get("error").asText();
+                log.error("Azkaban Execute a Flow {} failure: {}", flow, errorMessage);
+                throw new AzkabanException(errorMessage);
+            }
+        } catch (IOException e) {
+            log.error(String.format("Azkaban Execute a Flow %s failure: %s", flow, e.getMessage()), e);
+            throw new AzkabanException(e.getMessage());
+        }
     }
 
     public String executeFLow(String projectName, String flowId, Map<String, Object> optionalParams) {
@@ -246,36 +279,39 @@ public class AzkabanAdapter {
         return exchange.toString();
     }
 
-    public String cancelFlowExecution(String execId) {
-        HttpHeaders httpHeaders = getAzkabanHeaders();
-        httpHeaders.add("Accept", "text/plain;charset=utf-8");
+    public void cancelFlowExecution(String execId) {
         Map<String, Object> map = new HashMap<>();
         map.put("id", SESSION_ID);
         map.put("execid", execId);
 
-        ResponseEntity<String> exchange = restTemplate.exchange(config.getUrl() + "/executor?session.id={id}&ajax=cancelFlow&execid={execid}", HttpMethod.GET,
-                new HttpEntity<String>(httpHeaders), String.class, map);
-
-        log.info("Azkban cancel a Flow Execution:{}", exchange);
-        return exchange.toString();
+        String respResult = restTemplate.getForObject(config.getUrl() + "/executor?session.id={id}&ajax=cancelFlow&execid={execid}", String.class, map);
+        try {
+            JsonNode respRoot = objectMapper.readTree(respResult);
+            if (respRoot.hasNonNull("error")) {
+                String errorMessage = respRoot.get("error").asText();
+                log.error("Azkaban cancel a Execution {} failure: {}", execId, errorMessage);
+                throw new AzkabanException(errorMessage);
+            }
+        } catch (IOException e) {
+            log.error(String.format("Azkaban cancel a Execution %s failure: %s", execId, e.getMessage()), e);
+        }
     }
 
     /**
      * Schedule a period-based Flow
-     * @param projectName The name of the project
-     * @param flowName The name of the flow
+     *
+     * @param projectName  The name of the project
+     * @param flowName     The name of the flow
      * @param scheduleTime The time to schedule the flow. Example: 12,00,pm,PDT (Unless UTC is specified, Azkaban will take current server’s default timezone instead)
      * @param scheduleDate The date to schedule the flow. Example: 07/22/2014
-     * @param period Specifies the recursion period. Depends on the “is_recurring” flag being set. Example: 5w
+     * @param period       Specifies the recursion period. Depends on the “is_recurring” flag being set. Example: 5w
      */
     public void schedulePeriodBasedFlow(String projectName, String flowName, String scheduleDate, String scheduleTime, String period) {
         LinkedMultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("session.id", SESSION_ID);
         params.add("ajax", "scheduleFlow");
         params.add("projectName", projectName);
-        String projectId = Optional.ofNullable(fetchProjectFlows(projectName))
-                .map(pn -> pn.get("projectId").asText())
-                .orElseThrow(() -> new AzkabanException(String.format("项目 %s 不存在！", projectName)));
+        String projectId = Optional.ofNullable(fetchProjectFlows(projectName).get("projectId")).map(JsonNode::asText).orElse("");
         params.add("projectId", projectId);
         params.add("flow", flowName);
         params.add("scheduleTime", scheduleTime);
@@ -286,7 +322,6 @@ public class AzkabanAdapter {
         }
 
         HttpEntity<LinkedMultiValueMap<String, String>> httpEntity = new HttpEntity<>(params, getAzkabanHeaders());
-
         String respResult = restTemplate.postForObject(config.getUrl() + "/schedule", httpEntity, String.class);
 
         try {
@@ -314,10 +349,11 @@ public class AzkabanAdapter {
     }
 
     /**
+     * 调度一个基于 Cron 的 Flow
      *
-     * @param projectName
-     * @param flowName
-     * @param cronExpression
+     * @param projectName    项目名
+     * @param flowName       Flow 名
+     * @param cronExpression Cron 表达式
      */
     public void scheduleCronBasedFlow(String projectName, String flowName, String cronExpression) {
         LinkedMultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
@@ -340,7 +376,11 @@ public class AzkabanAdapter {
             } else {
                 String errorMessage = respRoot.hasNonNull("message")
                         ? respRoot.get("message").asText()
-                        : (respRoot.hasNonNull("error") ? respRoot.get("error").asText() : "No message.");
+                        : (
+                        respRoot.hasNonNull("error")
+                                ? respRoot.get("error").asText()
+                                : "No message."
+                );
                 log.error("Azkaban schedule Cron Flow {} failure: {}", flowName, errorMessage);
                 throw new AzkabanException(errorMessage);
             }
@@ -351,9 +391,31 @@ public class AzkabanAdapter {
     }
 
     /**
+     * 获取一个 Schedule
+     * @param projectId 项目 ID
+     * @param flowId Flow ID
+     * @return Schedule
+     */
+    public JsonNode fetchSchedule(String projectId, String flowId) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("id", SESSION_ID);
+        params.put("projectId", projectId);
+        params.put("flowId", flowId);
+
+        try {
+            String respResult = restTemplate.getForObject(config.getUrl() + "/schedule?session.id={id}&ajax=fetchSchedule&projectId={projectId}&flowId={flowId}", String.class, params);
+            return objectMapper.readTree(respResult);
+        } catch (IOException e) {
+            log.error(String.format("Azkaban fetch Schedule of Flow %s failure: %s", flowId, e.getMessage()), e);
+            throw new AzkabanException(e.getMessage());
+        }
+    }
+
+    /**
      * Flexible scheduling using Cron
-     * @param projectName The name of the project
-     * @param flowName The name of the flow
+     *
+     * @param projectName    The name of the project
+     * @param flowName       The name of the flow
      * @param cronExpression A CRON expression is a string comprising 6 or 7 fields separated by white space that represents a set of times
      * @return Response data
      */
@@ -377,7 +439,117 @@ public class AzkabanAdapter {
     }
 
     /**
+     * 移除一个调度
+     * @param scheduleId Schedule ID
+     */
+    public void unscheduleFlow(String scheduleId) {
+        LinkedMultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+        params.add("session.id", SESSION_ID);
+        params.add("action", "removeSched");
+        params.add("scheduleId", scheduleId);
+
+        HttpEntity<LinkedMultiValueMap<String, Object>> httpEntity = new HttpEntity<>(params, getAzkabanHeaders());
+
+        String respResult = restTemplate.postForObject(config.getUrl() + "/schedule", httpEntity, String.class);
+        try {
+            JsonNode respRoot = objectMapper.readTree(respResult);
+            if (respRoot.hasNonNull("status") && "success".equals(respRoot.get("status").asText())) {
+                log.info("Azkaban unschedule a Flow: {}", respRoot.hasNonNull("message") ? respRoot.get("message").asText() : "No message.");
+            } else {
+                String errorMessage = respRoot.hasNonNull("message")
+                        ? respRoot.get("message").asText()
+                        : (
+                        respRoot.hasNonNull("error")
+                                ? respRoot.get("error").asText()
+                                : "No message."
+                );
+                log.error("Azkaban unschedule Flow {} failure: {}", scheduleId, errorMessage);
+                throw new AzkabanException(errorMessage);
+            }
+        } catch (IOException e) {
+            log.error(String.format("Azkaban unschedule Flow %s failure: %s", scheduleId, e.getMessage()), e);
+            throw new AzkabanException(e.getMessage());
+        }
+    }
+
+    public void setSla(String scheduleId, String[] slaEmails, String[][] settings) {
+        LinkedMultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+        params.add("session.id", SESSION_ID);
+        params.add("ajax", "setSla");
+        params.add("scheduleId", scheduleId);
+        params.add("slaEmails", Optional.ofNullable(slaEmails).map(sm -> String.join(";", sm)).orElse(""));
+        for (int i = 0; i < settings.length; i++) {
+            params.add(String.format("settings[%d]", i), settings[i]);
+        }
+
+        HttpEntity<LinkedMultiValueMap<String, Object>> httpEntity = new HttpEntity<>(params, getAzkabanHeaders());
+
+        String respResult = restTemplate.postForObject(config.getUrl() + "/schedule", httpEntity, String.class);
+
+        try {
+            JsonNode respRoot = objectMapper.readTree(respResult);
+            if (respRoot.hasNonNull("error")) {
+                log.error("Azkaban set SLA for Scheduled {} failure: {}", scheduleId, respRoot.get("error").asText());
+                throw new AzkabanException(respRoot.get("error").asText());
+            }
+        } catch (IOException e) {
+            log.error(String.format("Azkaban set SLA for Scheduled %s failure: %s", scheduleId, e.getMessage()), e);
+            throw new AzkabanException(e.getMessage());
+        }
+    }
+
+    /**
+     * 暂停一个 Execution
+     * @param execid Execution ID
+     */
+    public void pauseFlowExecution(String execid) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", SESSION_ID);
+        map.put("execid", execid);
+
+        String respResult = restTemplate.getForObject(config.getUrl() + "/executor?session.id={id}&ajax=pauseFlow&execid={execid}", String.class, map);
+        try {
+            JsonNode respRoot = objectMapper.readTree(respResult);
+            if (!respRoot.hasNonNull("error")) {
+                log.info("Azkaban paused a Flow: %s", execid);
+            } else {
+                log.error("Azkaban pause Flow Execution {} failure: {}", execid, respRoot.get("error").asText());
+                throw new AzkabanException(respRoot.get("error").asText());
+            }
+        } catch (IOException e) {
+            log.error("Azkaban pause Flow Execution {} failure: {}", execid, e.getMessage());
+            throw new AzkabanException(e.getMessage());
+        }
+    }
+
+    /**
+     * 恢复一个 Execution
+     * @param execid Execution ID
+     */
+    public void resumeFlowExecution(String execid) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", SESSION_ID);
+        map.put("execid", execid);
+
+        String respResult = restTemplate.getForObject(config.getUrl() + "/executor?session.id={id}&ajax=resumeFlow&execid={execid}", String.class, map);
+
+        try {
+            JsonNode respRoot = objectMapper.readTree(respResult);
+            if (!respRoot.hasNonNull("error")) {
+                log.info("Azkaban resumed a Flow: {}", execid);
+            } else {
+                log.error("Azkaban resume Flow Execution {} failure: {}", execid, respRoot.get("error").asText());
+                throw new AzkabanException(respRoot.get("error").asText());
+            }
+        } catch (IOException e) {
+            log.error("Azkaban resume Flow Execution {} failure: {}", execid, e.getMessage());
+            throw new AzkabanException(e.getMessage());
+        }
+    }
+
+    /**
      * 执行 flow
+     *
      * @param projectName 项目名称
      * @param flowName    flow 名称
      * @return 执行 ID
@@ -396,6 +568,7 @@ public class AzkabanAdapter {
 
     /**
      * 执行信息
+     *
      * @param execId 执行ID
      * @return 结果
      */
@@ -409,25 +582,11 @@ public class AzkabanAdapter {
         return res;
     }
 
-    /**
-     * 执行job日志
-     * @param execId 执行ID
-     * @param jobId job ID
-     * @param offset 起始位置
-     * @param length 长度
-     * @return 结果
-     */
-    public String fetchExecutionJobLogs(String execId, String jobId, int offset, int length) {
-        String res = restTemplate
-                .getForObject(config.getUrl() + "/executor?ajax=fetchExecJobLogs&session.id={1}&execid={2}&jobId={3}&offset={4}&length={5}"
-                        , String.class, SESSION_ID, execId, jobId, offset, length
-                );
-        log.info("azkban execution job logs:{}", res);
-        return res;
-    }
+
 
     /**
      * 查询 flow 执行情况
+     *
      * @param execId 执行ID
      * @return 结果
      */
@@ -438,6 +597,24 @@ public class AzkabanAdapter {
                 );
         log.info("azkban execution flow:{}", res);
 
+        return res;
+    }
+
+    /**
+     * 执行job日志
+     *
+     * @param execId 执行ID
+     * @param jobId  job ID
+     * @param offset 起始位置
+     * @param length 长度
+     * @return 结果
+     */
+    public String fetchExecutionJobLogs(String execId, String jobId, int offset, int length) {
+        String res = restTemplate
+                .getForObject(config.getUrl() + "/executor?ajax=fetchExecJobLogs&session.id={1}&execid={2}&jobId={3}&offset={4}&length={5}"
+                        , String.class, SESSION_ID, execId, jobId, offset, length
+                );
+        log.info("azkban execution job logs:{}", res);
         return res;
     }
 
